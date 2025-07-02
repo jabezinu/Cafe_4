623687fc74aa67a069b2fb4fd40a22157ca5287e
jabezinu <jabezzinu31@gmail.com>
2025-07-02 11:16:50 +0300

    Comment Controller

---
+++ backend/app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  def index  # getcomment
+    comments = Comment.all
+    render json: comments
+  end
+
+  def create  # Createcomment
+    comment = Comment.new(comment_params)
+    if comment.save
+      render json: comment, status: :created
+    else
+      render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:name, :phone, :comment, :is_anonymous)
+  end
+end
